7d92211804929a9820511e23d9b98bfaf7ee2492
Robrecht Cannoodt <rcannood@gmail.com>
2024-11-19 10:48:01 +0100

    implement process_dataset component

--- scripts/create_resources/test_resources.sh
+++ scripts/create_resources/test_resources.sh
@@ -13,23 +13,70 @@ cd "$REPO_ROOT"
 
 set -e
 
+RAW_DATA=resources_test/task_cyto_batch_integration/starter_file
+
 DATASET_DIR=resources_test/task_cyto_batch_integration/starter_file
 
 mkdir -p $DATASET_DIR
 
-wget https://zenodo.org/records/13928969/files/ID1_Panel1_TP1.fcs?download=1 \
-  -O $DATASET_DIR/ID1_Panel1_TP1.fcs
+# TODO: get original_dataset.h5ad from somewhere
+
+# wget https://zenodo.org/records/13928969/files/ID1_Panel1_TP1.fcs?download=1 \
+#   -O $DATASET_DIR/ID1_Panel1_TP1.fcs
+
+# python << HERE
+# import readfcs
+# ad = readfcs.read("$DATASET_DIR/ID1_Panel1_TP1.fcs")
+# ad.layers["transformed"] = ad.X
+# del ad.X
+# # todo: add other preprocessing steps to make sure the dataset is a common dataset
+# ad.write_h5ad("$DATASET_DIR/common_dataset.h5ad")
+# HERE
 
 python << HERE
-import readfcs
+import anndata as ad
 
-ad = readfcs.read("$DATASET_DIR/ID1_Panel1_TP1.fcs")
-ad.layers["transformed"] = ad.X
-del ad.X
+adata = ad.read_h5ad("resources_test/task_cyto_batch_integration/starter_file/original_dataset.h5ad")
 
-# todo: add other preprocessing steps to make sure the dataset is a common dataset
+channelsofinterest = ['UV379-A',
+ 'UV515-A',
+ 'UV610-A',
+ 'UV735-A',
+ 'V431-A',
+ 'V525-A',
+ 'V586-A',
+ 'V605-A',
+ 'V677-A',
+ 'V710-A',
+ 'V750-A',
+ 'V810-A',
+ 'B530-A',
+ 'B710-A',
+ 'YG586-A',
+ 'YG610-A',
+ 'YG670-A',
+ 'YG780-A',
+ 'R670-A',
+ 'R730-A']
+adata.var.rename(columns={"n":"numeric_id"}, inplace=True)
+marker_types = ["lineage" if chan in channelsofinterest else 'functional' for chan in adata.var["channel"]]
+to_correct = [True if chan in channelsofinterest else False for chan in adata.var["channel"]]
+adata.var["marker_type"] = marker_types
+adata.var['to_correct'] = to_correct
+adata.uns['dataset_id'] = 'XXXXX'
+adata.uns['dataset_name'] = 'Summer School data'
+adata.uns['dataset_summary'] = 'Draft data for cytometry batch integration benchmark'
+adata.uns['dataset_description'] = '''
+This is a draft dataset for the cytometry batch integration benchmark (Summer School). 
+It contains only samples from one batch (Day1). 
+Even though a preprocessed layer is available, it only contains arcsinh transformed data (not cleaned or compensated data).
+'''
+adata.uns['dataset_url'] = "https://saeyslab.sites.vib.be"
+adata.uns['dataset_organism'] = "mus_musculus"
+adata.uns['dataset_reference'] = "unpublished"
 
-ad.write_h5ad("$DATASET_DIR/common_dataset.h5ad")
+out_file = "resources_test/task_cyto_batch_integration/starter_file/common_dataset.h5ad"
+adata.write_h5ad(out_file, compression="gzip")
 HERE
 
 # process dataset
@@ -37,8 +84,7 @@ viash run src/data_processors/process_dataset/config.vsh.yaml -- \
   --input $RAW_DATA/common_dataset.h5ad \
   --output_unintegrated $DATASET_DIR/unintegrated.h5ad \
   --output_unintegrated_censored $DATASET_DIR/unintegrated_censored.h5ad \
-  --output_validation $DATASET_DIR/validation.h5ad \
-  --validation_sample_names "sample1;sample2;sample3"
+  --output_validation $DATASET_DIR/validation.h5ad
 
 # # run one method
 # viash run src/methods/logistic_regression/config.vsh.yaml -- \
