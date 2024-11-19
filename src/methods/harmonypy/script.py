5043e5052165b0bc13cde51ab7845da927869f32
Robrecht Cannoodt <rcannood@gmail.com>
2024-11-19 11:22:37 +0100

    add harmonypy method

--- src/methods/harmonypy/script.py
+++ src/methods/harmonypy/script.py
@@ -1,4 +1,5 @@
 import anndata as ad
+import harmonypy
 
 ## VIASH START
 par = {
@@ -13,17 +14,27 @@ meta = {
 print('Reading input files', flush=True)
 input = ad.read_h5ad(par['input'])
 
-print('Preprocess data', flush=True)
-# ... preprocessing ...
-
-print('Train model', flush=True)
-# ... train model ...
-
-print('Generate predictions', flush=True)
-# ... generate predictions ...
+print('Run harmony', flush=True)
+# harmony can't handle integer batch labels
+input.obs["batch_str"] = input.obs["batch"].astype(str)
+out = harmonypy.run_harmony(
+  input.layers["preprocessed"],
+  input.obs,
+  "batch_str"
+)
 
 print("Write output AnnData to file", flush=True)
 output = ad.AnnData(
-  
+  obs=input.obs[[]],
+  var=input.var[[]],
+  layers={
+    "integrated": out.Z_corr.transpose()
+  },
+  uns={
+    "dataset_id": input.uns["dataset_id"],
+    "method_id": meta["name"],
+    "parameters": {}
+  }
 )
+
 output.write_h5ad(par['output'], compression='gzip')
