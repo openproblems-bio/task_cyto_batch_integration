50ba3bf2cacd4d63edcf554f459a51a642403cbb
Robrecht Cannoodt <rcannood@gmail.com>
2024-12-04 10:10:14 +0100

    add harmonypy (#3)
    * add skeleton files
    
    * add harmonypy method
    
    * add harmonypy implementation
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Givanna Putri <ghar1821@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,21 +6,11 @@
 
 ## NEW FUNCTIONALITY
 
-* Added `control_methods/true_labels` component (PR #5).
-
-* Added `methods/logistic_regression` component (PR #5).
-
-* Added `metrics/accuracy` component (PR #5).
+* Added `methods/harmonypy` component (PR #3).
 
 ## MAJOR CHANGES
 
-* Updated `api` files (PR #5).
-
-* Updated configs, components and CI to the latest Viash version (PR #8).
-
 ## MINOR CHANGES
-
-* Updated `README.md` (PR #5).
 
 ## BUGFIXES
 
---
+++ src/methods/harmonypy/script.py
@@ -0,0 +1,45 @@
+import anndata as ad
+import harmonypy
+
+## VIASH START
+par = {
+    "input": "resources_test/task_cyto_batch_integration/starter_file/unintegrated_censored.h5ad",
+    "output": "output.h5ad",
+}
+meta = {"name": "harmonypy"}
+## VIASH END
+
+print("Reading and preparing input files", flush=True)
+adata = ad.read_h5ad(par["input"])
+
+adata.obs["batch_str"] = adata.obs["batch"].astype(str)
+
+markers_to_correct = adata.var[adata.var["to_correct"]].index.to_numpy()
+
+adata_to_correct = adata[:, markers_to_correct]
+
+print("Run harmony", flush=True)
+# harmony can't handle integer batch labels
+
+out = harmonypy.run_harmony(
+    data_mat=adata_to_correct.layers["preprocessed"],
+    meta_data=adata_to_correct.obs,
+    vars_use="batch_str",
+)
+
+# create new anndata
+out_adata = ad.AnnData(
+    obs=adata.obs,
+    var=adata_to_correct.var,
+    layers={"integrated": out.Z_corr.transpose()},
+    uns={
+        "dataset_id": adata.uns["dataset_id"],
+        "method_id": meta["name"],
+        "parameters": {},
+    },
+)
+
+print("Write output AnnData to file", flush=True)
+
+# TODO need compression to gz or not?
+out_adata.write_h5ad(par["output"])
