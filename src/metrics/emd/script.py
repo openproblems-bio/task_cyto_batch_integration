0423799b338abe490764d3403608758eb58041ec
Givanna Putri <ghar1821@users.noreply.github.com>
2025-02-27 20:43:53 +1100

    Update emd (#27)
    * update emd max
    
    * update methods to return all vars
    
    * add mean emd
    
    * Update CHANGELOG.md
    
    * Revert "update methods to return all vars"
    
    This reverts commit dd04149cd10affdfbcd9c5eefee0ee3f05697f41.
    
    The commit is applied to a separate branch to split the 2 major changes.
    
    * Update CHANGELOG.md
    
    * merge emd_max and emd_mean into one
    
    * remove sample_ids from metric output
    
    * fix workflow
    
    * trigger ci
    
    ---------
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>

--- src/metrics/emd/script.py
+++ src/metrics/emd/script.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+import sys
 
 import anndata as ad
 import cytonormpy as cnp
@@ -17,143 +17,82 @@ par = {
 meta = {"name": "emd_per_samples"}
 ## VIASH END
 
+sys.path.append(meta["resources_dir"])
+from helper_functions import concatenate_inputs
+
 print("Reading input files", flush=True)
 
 input_integrated = ad.read_h5ad(par["input_integrated"])
 input_unintegrated = ad.read_h5ad(par["input_unintegrated"])
 input_validation = ad.read_h5ad(par["input_validation"])
 
-# add all obs columns in the unintegrated data to integrated data
-# loc should order the obs based on obs_names
-input_integrated.obs = input_unintegrated.obs.loc[input_integrated.obs_names]
-
-# TODO uncomment me if you want to have some samples in validation but not in integrated
-# input_validation = input_integrated[
-#     input_integrated.obs["sample"] == "Tube1_Batch2_WT"
-# ].copy()
-# input_validation.layers["preprocessed"] = input_validation.layers["integrated"]
-# del input_validation.layers["integrated"]
-# input_integrated = input_integrated[
-#     input_integrated.obs["sample"].isin(
-#         ["Tube1_Batch1_WT", "Tube2_Batch1_KO", "Tube2_Batch2_KO"]
-#     )
-# ].copy()
+input_concat = concatenate_inputs(
+    input_integrated=input_integrated,
+    input_unintegrated=input_unintegrated,
+    input_validation=input_validation
+)
 
 markers_to_assess = input_unintegrated.var[
     input_unintegrated.var["to_correct"]
 ].index.to_numpy()
 
-print("Extracting samples to compute the metric for", flush=True)
+# keep only markers we need to assess
+input_concat = input_concat[:, markers_to_assess]
 
-# Perhaps overengineering this but the idea is to get all the sample names from
-# the integrated data, get the donors name from the combination of input_validation
-# and input_unintegrated. Why? because depending on how we structure the input
-# for an algorithm we may include matching samples from same donor (which will then
-# appear in input_unintegrated) or not (which will then only appear in input_validation).
-# then get all samples belong to that donor, double check the batch id,
-# and compute the emd across the matching samples
+# remove control samples as we won't be assessing these
+input_concat = input_concat[input_concat.obs['is_control'] == 0]
 
-# get all donors-sample map from data
-# for some dataset "scheme", matching samples from the same donor may not be given
-# as an input to the batch correction algorithm.
-# thus they will only exist in validation. we need this for each donor processed
-# by the batch correction algorithm.
-# Assumption here is that for a given donor, there must be at least 1 sample
-# processed by the batch correction algorithm, i.e., in uncorrected_censored (or uncorrected).
-sample_donor_map_arr = np.concatenate(
-    (
-        np.unique(
-            input_validation.obs[["sample", "donor"]].to_numpy().astype(str), axis=0
-        ),
-        np.unique(
-            input_integrated.obs[["sample", "donor"]].to_numpy().astype(str), axis=0
-        ),
-    )
-)
-# keep just the donors which samples were processed by the batch correction algorithm.
-# Why? we only need to compute the metric score for matching samples for donors that
-# are processed by the batch correction algorithm.
-# TODO can use list instead of set, because validation and unintegrated should not
-# share common samples? Keep set for now.
-sample_donor_dict = defaultdict(set)
+# needed to assemble the output anndata later
+dataset_id = input_integrated.uns["dataset_id"]
+method_id = input_integrated.uns["method_id"]
 
-for sample_donor_pair in sample_donor_map_arr:
-    sample = sample_donor_pair[0]
-    donor = sample_donor_pair[1]
-    # print("sample: %s, donor: %s" % (sample, donor))
-    sample_donor_dict[donor].add(sample_donor_pair[0])
+# shouldn't need these anymore
+del input_unintegrated
+del input_integrated
+del input_validation
 
-emd_integrated_per_donor = []
-samples_for_emd = []
+donors = input_concat.obs['donor'].unique()
 
-for donor, samples in sample_donor_dict.items():
+emd_df = []
+emd_vals = []
 
-    # test 1 donor for now.
-    # donor = list(sample_donor_dict.keys())[0]
-    # samples = sample_donor_dict[donor]
-
-    # the data for some samples may only be in the validation because they were not
-    # included as input for the batch correction algorithm.
-    input_integrated_donor = input_integrated[
-        input_integrated.obs["sample"].isin(samples)
-    ]
-    input_validation_donor = input_validation[
-        input_validation.obs["sample"].isin(samples)
-    ]
-
-    # keep just the markers we need to assess
-    input_integrated_donor = input_integrated_donor[:, markers_to_assess]
-    input_validation_donor = input_validation_donor[:, markers_to_assess]
-
-    input_for_emd = ad.concat([input_integrated_donor, input_validation_donor])
-    input_for_emd.layers["integrated"] = np.concatenate(
-        (
-            input_integrated_donor.layers["integrated"],
-            input_validation_donor.layers["preprocessed"],
-        )
-    )
-
-    # sanity check.. we need to have at least 2 batches..
-    if len(np.unique(input_for_emd.obs["batch"])) < 2:
-        raise Exception(
-            "The samples to calculate EMD_per_sample for donor %s only belong to 1 batch?"
-            % donor
-        )
-
-    print("Computing EMD for donor %s" % donor, flush=True)
+for donor in donors:
+    # donor = donors[0]
+    input_donor = input_concat[input_concat.obs['donor'] == donor]
 
     # have to change the "sample" column to file_name for emd_comparison_from_anndata to work.
     # Otherwise the _calculate_emd_per_frame used in cytonormpy will error because they
     # harcoded the column file_name and use it in assert.
     # See line 176 of https://github.com/TarikExner/CytoNormPy/blob/main/cytonormpy/_evaluation/_emd_utils.py#L173
-    input_for_emd.obs["file_name"] = input_for_emd.obs["sample"]
+    input_donor.obs.rename(columns={'sample': 'file_name'}, inplace=True)
 
     emd_integrated = cnp.emd_from_anndata(
-        adata=input_for_emd,
-        file_list=list(samples),
+        adata=input_donor,
+        file_list=list(input_donor.obs['file_name'].unique()),
         channels=markers_to_assess,
-        layer="integrated",
+        layer="data",
         sample_identifier_column="file_name",
-    ).reset_index()
-
-    del emd_integrated["label"]
-
-    emd_integrated_per_donor.append(emd_integrated)
-    samples_for_emd.append(";".join(samples))
-
-emd_integrated_per_donor = pd.concat(emd_integrated_per_donor, ignore_index=True)
-
-max_emd = np.max(emd_integrated_per_donor)
-# mean_emd = np.mean(emd_integrated_per_donor)
+        cell_labels="cell_type"
+    )
+    
+    emd_integrated['donor'] = donor
+    emd_df.append(emd_integrated.copy())
+    
+    # ignoring unlabeled and all_cells
+    emd_integrated.drop(index=['all_cells', 'Unlabeled'], inplace=True)
+    
+    emd_vals.extend(emd_integrated[markers_to_assess].to_numpy().flatten())
+    
+emd_df = pd.concat(emd_df)
 
 print("Assembling output AnnData", flush=True)
 output = ad.AnnData(
     uns={
-        "dataset_id": input_integrated.uns["dataset_id"],
-        "method_id": input_integrated.uns["method_id"],
-        "metric_ids": ["max_emd"],
-        "metric_values": [max_emd],
-        "sample_ids": samples_for_emd,
+        "dataset_id": dataset_id,
+        "method_id": method_id,
+        "metric_ids": ["emd_mean", "emd_max"],
+        "metric_values": [np.nanmean(emd_vals), np.nanmax(emd_vals)],
+        "emd_values": emd_df
     }
 )
 
