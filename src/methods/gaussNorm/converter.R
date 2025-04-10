28c4274d6acc293a32ed41d6cdc29235f17cc964
Luqui12 <luca.leomazzi@gmail.com>
2025-04-10 17:11:58 +0200

    Method implemented. `converter.R` modified

--- src/methods/gaussNorm/converter.R
+++ src/methods/gaussNorm/converter.R
@@ -4,10 +4,11 @@ library(Biobase)
 
 # Take a h5ad file as an input and return a collection of fcs for each sample in the Anndata object
 # NOTE: the AnnData object must have valid adata$obs$sample and adata$var$channel fields.
-# Example usage : `write_fcs_from_h5ad('mydata.h5ad',"path_to_dir")`
+# NOTE: the AnnData object must have a layer called "preprocessed" that contains the data to be written to fcs files.
+# Example usage : `write_fcs_from_h5ad("path_to_h5ad","path_to_dir")`
 
-write_fcs_from_h5ad <- function(h5ad_file, out_dir) {
-  adata <- anndata::read_h5ad(h5ad_file)
+write_fcs_from_h5ad <- function(myfile, out_dir) {
+  adata <- anndata::read_h5ad(myfile)
   
   channel_names <- adata$var$channel
   sampl_names <- unique(adata$obs$sample)
@@ -24,7 +25,7 @@ write_fcs_from_h5ad <- function(h5ad_file, out_dir) {
   
   for (sample in sampl_names) {
     idx <- adata$obs$sample == sample
-    events <- as.matrix(adata$layers$preprocessed[idx, ])
+    events <- as.matrix(adata$layers[['preprocessed']][idx, ])
     colnames(events) <- channel_names
     ff <- flowFrame(exprs = events, parameters = par_df)
     
@@ -34,4 +35,6 @@ write_fcs_from_h5ad <- function(h5ad_file, out_dir) {
     filename <- paste0(out_dir,"/",sample, ".fcs")
     write.FCS(ff, filename)
   }
+  cat("FCS files created:\n")
+  return(list.files(out_dir, pattern = '*.fcs'))
 }
