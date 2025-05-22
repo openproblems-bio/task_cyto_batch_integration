8c9d7dac5bb329aa1b788a76154f7035ae4b83b4
Robrecht Cannoodt <rcannood@gmail.com>
2025-05-22 04:52:35 +0200

    Update scripts (#57)
    * Fix scripts
    
    * update common
    
    ---------
    
    Co-authored-by: Givanna Putri <givanna.h@gmail.com>

--- scripts/run_benchmark/run_full_local.sh
+++ scripts/run_benchmark/run_full_local.sh
@@ -6,19 +6,6 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# NOTE: depending on the the datasets and components, you may need to launch this workflow
-# on a different compute platform (e.g. a HPC, AWS Cloud, Azure Cloud, Google Cloud).
-# please refer to the nextflow information for more details:
-# https://www.nextflow.io/docs/latest/
-
-# remove this when you have implemented the script
-echo "TODO: once the 'run_benchmark' workflow has been implemented, update this script to use it."
-echo "  Step 1: replace 'task_cyto_batch_integration' with the name of the task in the following command."
-echo "  Step 2: replace the rename keys parameters to fit your run_benchmark inputs"
-echo "  Step 3: replace the settings parameter to fit your run_benchmark outputs"
-echo "  Step 4: remove this message"
-exit 1
-
 set -e
 
 echo "Running benchmark on test data"
@@ -31,7 +18,7 @@ publish_dir="resources/results/${RUN_ID}"
 # write the parameters to file
 cat > /tmp/params.yaml << HERE
 input_states: resources/datasets/**/state.yaml
-rename_keys: 'input_train:output_train;input_test:output_test;input_solution:output_solution'
+rename_keys: 'input_unintegrated_censored:output_unintegrated_censored;input_unintegrated:output_unintegrated;input_validation:output_validation'
 output_state: "state.yaml"
 publish_dir: "$publish_dir"
 HERE
