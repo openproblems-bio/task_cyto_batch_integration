cbef7eca28fda1dd32c24874515e85acebae91ef
Givanna Putri <givanna.h@gmail.com>
2025-06-24 20:14:48 +1000

    added cycombnie with healthy controls only

--- src/methods/cycombine_healthy_controls/script.R
+++ src/methods/cycombine_healthy_controls/script.R
@@ -1,12 +1,12 @@
-library(anndata)
 requireNamespace("cyCombine", quietly = TRUE)
+requireNamespace("anndata", quietly = TRUE)
 
 ## VIASH START
 par <- list(
-    input = "resources_test/task_cyto_batch_integration/mouse_spleen_flow_cytometry_subset/unintegrated_censored.h5ad",
-    output = "resources_test/task_cyto_batch_integration/mouse_spleen_flow_cytometry_subset/output.h5ad"
+    input = "resources_test/task_cyto_batch_integration/leomazzi_cyto_spleen_subset/unintegrated_censored.h5ad",
+    output = "resources_test/task_cyto_batch_integration/leomazzi_cyto_spleen_subset/output.h5ad"
 )
-meta <- list(name = "cycombine")
+meta <- list(name = "cycombine_healthy_controls")
 ## VIASH END
 
 cat("Reading input files\n")
@@ -14,19 +14,37 @@ input_adata <- anndata::read_h5ad(par[["input"]])
 
 cat("Preparing input Anndata and df\n")
 
-adata_to_correct <- input_adata[, input_adata$var$to_correct]
-
 markers_to_correct <- input_adata$var_names[input_adata$var$to_correct]
 
-lineage_markers <- as.vector(input_adata$var_names[
-    input_adata$var$marker_type == "lineage"
-])
+adata_to_correct <- input_adata[, markers_to_correct]
+
+# convert adata to data.frame
 
 df_to_correct <- as.data.frame(
     adata_to_correct$layers[["preprocessed"]]
 )
 df_to_correct$batch <- adata_to_correct$obs$batch
 df_to_correct$sample <- adata_to_correct$obs$sample
+
+# add an "anchor" column which specify which samples are the technical replicates
+# this is a bit weird as the anchor information should be a column name or character vector
+# giving all anchors the same label and every other sample a unique label.
+# so for our healthy controls, i have to give it the same name, but unique for others..
+# as character is needed as otherwise we will get NAs for those healthy controls..
+df_to_correct$anchor <- as.character(adata_to_correct$obs$sample)
+
+# get sample name for healthy controls
+healthy_controls_sample_name <- as.vector(
+    unique(adata_to_correct$obs[adata_to_correct$obs$is_control == 1, "sample"])
+)
+
+# change the "anchor" column values for this healthy controls
+df_to_correct$anchor[df_to_correct$anchor %in% healthy_controls_sample_name] <- "control"
+
+lineage_markers <- as.vector(input_adata$var_names[
+    input_adata$var$marker_type == "lineage"
+])
+
 
 cat("Run cyCombine\n")
 
@@ -56,7 +74,7 @@ df_corrected <- cyCombine::correct_data(
     markers = markers_to_correct,
     method = "ComBat",
     covar = NULL,
-    anchor = NULL,
+    anchor = "anchor",
     ref.batch = NULL,
     parametric = TRUE
 )
