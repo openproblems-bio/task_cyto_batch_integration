ec034b5aac62f0c2a5f309fb98121f7a1e0c5e66
Luqui12 <luca.leomazzi@gmail.com>
2025-06-30 12:00:27 +0200

    Initial commit

---
+++ src/methods/mnn/script.R
@@ -0,0 +1,29 @@
+library(anndata)
+
+## VIASH START
+par <- list(
+  input = "resources_test/.../input.h5ad",
+  output = "output.h5ad"
+)
+meta <- list(
+  name = "mnn"
+)
+## VIASH END
+
+cat("Reading input files\n")
+input <- anndata::read_h5ad(par[["input"]])
+
+cat("Preprocess data\n")
+# ... preprocessing ...
+
+cat("Train model\n")
+# ... train model ...
+
+cat("Generate predictions\n")
+# ... generate predictions ...
+
+cat("Write output AnnData to file\n")
+output <- anndata::AnnData(
+  
+)
+output$write_h5ad(par[["output"]], compression = "gzip")
