42e8bd21b81e5cef28d1b61489ca9e9d5828afe1
Givanna Putri <givanna.h@gmail.com>
2025-10-14 14:51:10 +1100

    downgrading image

--- scripts/run_benchmark/run_full_seqeracloud.sh
+++ scripts/run_benchmark/run_full_seqeracloud.sh
@@ -17,16 +17,16 @@ cat > /tmp/params.yaml << HERE
 input_states: s3://openproblems-data/resources/task_cyto_batch_integration/datasets/**/state.yaml
 rename_keys: 'input_censored_split1:output_censored_split1;input_censored_split2:output_censored_split2;input_unintegrated:output_unintegrated'
 output_state: "state.yaml"
-settings: '{"metrics_exclude": ["cms"], "methods_include": ["mnnpy", "cytovi"]}'
+settings: '{"metrics_include": ["emd", "ratio_inconsistent_peaks", "n_inconsistent_peaks"], "methods_include": ["harmonypy", "cycombine_no_controls_to_goal", "cycombine_all_controls_to_goal", "cytonorm_no_controls_to_goal", "cytonorm_all_controls_to_goal"]}'
 publish_dir: "$publish_dir"
 HERE
 
 tw launch https://github.com/openproblems-bio/task_cyto_batch_integration.git \
-  --revision build/fix_failed_stuff \
+  --revision build/update_n_inconsistent_peak \
   --pull-latest \
   --main-script target/nextflow/workflows/run_benchmark/main.nf \
   --workspace 53907369739130 \
   --params-file /tmp/params.yaml \
   --entry-name auto \
   --config common/nextflow_helpers/labels_tw.config \
-  --labels task_cyto_batch_integration,mnnnpy
+  --labels task_cyto_batch_integration,test_subset
