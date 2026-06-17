4945b1b67955963e8441b161ce22833e1627ff7d
Givanna Putri <givanna.h@gmail.com>
2026-06-17 15:19:50 +1000

    update description

--- scripts/run_metric_adhoc.sh
+++ scripts/run_metric_adhoc.sh
@@ -7,5 +7,5 @@ python "$REPO_ROOT/scripts/run_metric_adhoc.py" \
     --metric functional_marker_preservation \
     --input-dir /Users/putri.g/Documents/cytobenchmark/analysis/run_2026-04-26_21-34-33 \
     --datasets-dir /Users/putri.g/Documents/cytobenchmark/datasets \
-    --output-dir /Users/putri.g/Documents/cytobenchmark/analysis/analysis_2026-05-13/functional_marker_preservation \
+    --output-dir /Users/putri.g/Documents/cytobenchmark/analysis/analysis_2026-06-17/functional_marker_preservation \
     --skip-existing
